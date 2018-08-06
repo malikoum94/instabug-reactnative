08f7396d8f1c01ebb85a5ad2f1d025952dfe838a
malikoum94 <mmazouz@student.42.fr>
2018-08-06 15:51:11 +0200

    try a fix for java.lang.RuntimeException

--- utils/InstabugUtils.js
+++ utils/InstabugUtils.js
@@ -1,48 +1,94 @@
-'use strict';
-import {NativeModules, Platform} from 'react-native';
-let {Instabug} = NativeModules;
-import parseErrorStackLib from '../../react-native/Libraries/Core/Devtools/parseErrorStack.js';
+'use strict'
+import { NativeModules, Platform } from 'react-native'
+let { Instabug } = NativeModules
+import parseErrorStackLib from 'react-native/Libraries/Core/Devtools/parseErrorStack.js'
 
-let parseErrorStack = (error) => {
-    return parseErrorStackLib(error);
-};
+let parseErrorStack = error => {
+  try {
+    return parseErrorStackLib(error)
+  } catch (error2) {
+    var jsonObject = {
+      message: error2.name + ' - ' + error2.message,
+      os: Platform.OS,
+      platform: 'react_native',
+      exception: null,
+    }
+    if (Platform.OS === 'android') {
+      Instabug.sendJSCrash(JSON.stringify(jsonObject))
+    } else {
+      Instabug.sendJSCrash(jsonObject)
+    }
+    if (originalHandler) {
+      if (Platform.OS === 'ios') {
+        originalHandler(e, isFatal)
+      } else {
+        setTimeout(() => {
+          originalHandler(e, isFatal)
+        }, 500)
+      }
+    }
+  }
+}
 
-const originalHandler = global.ErrorUtils.getGlobalHandler();
+const originalHandler = global.ErrorUtils.getGlobalHandler()
 
 let init = () => {
-    if (__DEV__) {
-        return;
-    }
+  if (__DEV__) {
+    return
+  }
 
-    function errorHandler(e, isFatal) {
-      let jsStackTrace = parseErrorStackLib(e);
+  function errorHandler(e, isFatal) {
+    try {
+      let jsStackTrace = parseErrorStackLib(e)
 
       //JSON object to be sent to the native SDK
       var jsonObject = {
-        message: e.name + " - " + e.message,
+        message: e.name + ' - ' + e.message,
         os: Platform.OS,
         platform: 'react_native',
-        exception: jsStackTrace
+        exception: jsStackTrace,
       }
-      if(Platform.OS === 'android') {
-        Instabug.sendJSCrash(JSON.stringify(jsonObject));
+      if (Platform.OS === 'android') {
+        Instabug.sendJSCrash(JSON.stringify(jsonObject))
       } else {
-        Instabug.sendJSCrash(jsonObject);
+        Instabug.sendJSCrash(jsonObject)
       }
       if (originalHandler) {
         if (Platform.OS === 'ios') {
-          originalHandler(e, isFatal);
+          originalHandler(e, isFatal)
         } else {
           setTimeout(() => {
-            originalHandler(e, isFatal);
-          }, 500);
+            originalHandler(e, isFatal)
+          }, 500)
+        }
+      }
+    } catch (error) {
+      var jsonObject = {
+        message: error.name + ' - ' + error.message,
+        os: Platform.OS,
+        platform: 'react_native',
+        exception: null,
+      }
+      if (Platform.OS === 'android') {
+        Instabug.sendJSCrash(JSON.stringify(jsonObject))
+      } else {
+        Instabug.sendJSCrash(jsonObject)
+      }
+      if (originalHandler) {
+        if (Platform.OS === 'ios') {
+          originalHandler(e, isFatal)
+        } else {
+          setTimeout(() => {
+            originalHandler(e, isFatal)
+          }, 500)
         }
       }
     }
-    global.ErrorUtils.setGlobalHandler(errorHandler);
-};
+  }
+  global.ErrorUtils.setGlobalHandler(errorHandler)
+}
 
 module.exports = {
-    parseErrorStack: parseErrorStack,
-    captureJsErrors: init
-};
+  parseErrorStack: parseErrorStack,
+  captureJsErrors: init,
+}
